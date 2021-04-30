5b5afd08f864796ee984bc309b9b60b23a00fb58
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-05-01 01:30:51 +0200

    added class FileDialogViewModel

---
+++ Music Player/Music Player/ViewModels/FileDialogViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Win32;
+using Music_Player.Models;
+
+namespace Music_Player.ViewModels
+{
+    class FileDialogViewModel
+    {
+
+        void OpenFileDialog(PlaylistModel playlist)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "MP3 files (*.mp3)|*.mp3|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+               // mediaPlayer.Open(new Uri(openFileDialog.FileName));
+               // mediaPlayer.Play();
+            }
+        }
+    }
+}
