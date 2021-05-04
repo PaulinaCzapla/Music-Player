fb492a0313c7774f37bb660d0164755056daeae1
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-05-04 18:55:08 +0200

    adding new folder to TreeView

--- Music Player/Music Player/Views/MusicLibraryView.xaml.cs
+++ Music Player/Music Player/Views/MusicLibraryView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,11 +24,58 @@ namespace Music_Player.Views
         {
             InitializeComponent();
             DataContext = new Music_Player.ViewModels.MusicLibraryViewModel();
+
+           // var t = new TreeViewItem();
+
+
         }
 
         private void ButtonLibrary_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+
+            CreateFolderDialogView inputDialog = new CreateFolderDialogView();
+
+
+            if (inputDialog.ShowDialog() == true)
+            {
+                Debug.WriteLine("added new folder");
+
+                var item = new TreeViewItem();
+                
+                item.Header = inputDialog.folderNameInput.Text;
+                FolderView.Items.Add(item);
+            }
+
+            //string[] paths, files;
+
+            //OpenFileDialog openFileDialog = new OpenFileDialog();
+            //openFileDialog.Multiselect = true;
+
+            //if (openFileDialog.ShowDialog() == true)
+            //{
+            //    files = openFileDialog.SafeFileNames;
+            //    paths = openFileDialog.FileNames;
+
+            //    for (int i = 0; i < files.Length; i++)
+            //    {
+            //        var item = new TreeViewItem();
+
+            //        item.Header = files[i];
+
+            //        FolderView.Items.Add(item);
+            //   }
+
+            // }
         }
     }
 }
