3f7c93f8d81d81b4713b75a57f356495d0990897
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-05-05 03:50:06 +0200

    added a constructor and fields

--- Music Player/Music Player/Models/PlaylistModel.cs
+++ Music Player/Music Player/Models/PlaylistModel.cs
@@ -6,5 +6,35 @@ namespace Music_Player.Models
 {
     class PlaylistModel
     {
+        public string PlaylistName;
+        private List<SongModel> _Songs;
+        public List<SongModel> Songs
+        {
+            get { return _Songs; }
+            private set
+            {
+                _Songs = value;
+            }
+        }
+
+        private CoverModel _Cover;
+        public CoverModel Cover
+        {
+            get { return _Cover; }
+            private set
+            {
+                _Cover = value;
+            }
+        }
+
+
+        public PlaylistModel(string playlistName, List<SongModel> songs, string coverPath)
+        {
+            PlaylistName = playlistName;
+            Songs = songs;
+            Cover = new CoverModel(coverPath);
+        }
+
+
     }
 }
