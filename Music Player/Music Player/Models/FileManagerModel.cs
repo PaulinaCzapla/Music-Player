601e091b71c984cba7be3cd2af570bdf2ad7abae
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-05-05 03:48:24 +0200

    Create FileManagerModel.cs

---
+++ Music Player/Music Player/Models/FileManagerModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Music_Player.Models
+{
+    class FileManagerModel
+    {
+        private string Filename;
+
+        public FileManagerModel(string filename = "userplaylists")
+        {
+            Filename = filename;
+        }
+
+        public void ReadFile() { }
+
+        public void SaveFile() { }
+    }
+}
