71fc17c825907a0807a529b93f65202acd1183a5
PaulinaCzapla <paulcza866@student.polsl.pl>
2021-05-19 22:11:28 +0200

    added events handling and timer

--- Music Player/Music Player/ViewModels/PlayerStateViewModel.cs
+++ Music Player/Music Player/ViewModels/PlayerStateViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Music_Player.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Windows.Threading;
 
@@ -9,13 +10,37 @@ namespace Music_Player.ViewModels
     class PlayerStateViewModel
     {
         public PlaylistModel CurrentPlaylist;
-        public PlayerActions State { get; set; }
-        public string CurrentSongPath { get; set; }
+
+        private PlayerActions _State;
+        public PlayerActions State 
+        { 
+            get  { return _State; }
+            set 
+            {
+                StateChanged?.Invoke(this, value);
+                _State = value;
+            } 
+        }
+
+        private string _CurrentSongPath;
+        public string CurrentSongPath 
+        { 
+            get { return _CurrentSongPath; }
+            set
+            {
+                Debug.WriteLine("new song");
+                if (value != _CurrentSongPath)
+                {
+                   NewSongPlayed?.Invoke(this, value);
+                }
+                    _CurrentSongPath = value;
+            }
+        }
         public DispatcherTimer PlayTimer { get; set; }
 
         public event EventHandler PlayerTimerTicked;
-        public event EventHandler<string> NewSongPlayed;
-        public event EventHandler<string> StateChanged;
+        private event EventHandler<string> NewSongPlayed;
+        private event EventHandler<PlayerActions> StateChanged;
 
         public PlayerStateViewModel()
         {
@@ -23,31 +48,41 @@ namespace Music_Player.ViewModels
             State = PlayerActions.NotPlaying;
             CurrentSongPath = null;
             PlayTimer = null;
+
+           NewSongPlayed += NewSong_Play;
+            StateChanged += State_Change;
+
         }
         public void PlayTimer_Tick(object sender, EventArgs e)
         {
-            //progress bar and timer
+            //progress bar and timer handling
+            Debug.WriteLine(PlayerControlsViewModel.Player.Position.ToString(@"mm\:ss"));
         }
 
         public void NewSong_Play(object sender, string newPath)
         {
-            CurrentSongPath = newPath;
             PlayTimer = new DispatcherTimer();
-            PlayTimer.Interval = TimeSpan.FromMilliseconds(1000);
-            PlayTimer.Tick += new EventHandler(PlayTimer_Tick);
+            PlayTimer.Interval = TimeSpan.FromSeconds(1);
+            PlayTimer.Tick += PlayTimer_Tick;
             PlayTimer.Start();
+            Debug.WriteLine("startnew");
+
+            
+
         }
 
         public void State_Change(object sender, PlayerActions newState)
         {
             if (newState == PlayerActions.Pause)
             {
-                PlayTimer.Stop();
+                PlayTimer?.Stop();
+                Debug.WriteLine("stop");
             }
             else
              if (newState == PlayerActions.Play)
             {
-                PlayTimer.Start();
+                PlayTimer?.Start();
+                Debug.WriteLine("start");
             }
         }
     }
